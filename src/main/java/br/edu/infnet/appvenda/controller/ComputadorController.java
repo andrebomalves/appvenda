943953c36270b8f1f5d01e5c8ded41be4a637d55
Andre <andrebomalves@msn.com>
2023-11-12 21:40:58 -0300

    ajuste de imports

--- src/main/java/br/edu/infnet/appvenda/controller/ComputadorController.java
+++ src/main/java/br/edu/infnet/appvenda/controller/ComputadorController.java
@@ -2,12 +2,10 @@ package br.edu.infnet.appvenda.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import br.edu.infnet.appvenda.model.service.ComputadorService;
-import br.edu.infnet.appvenda.model.service.VendedorService;
 
 @Controller
 public class ComputadorController {	
